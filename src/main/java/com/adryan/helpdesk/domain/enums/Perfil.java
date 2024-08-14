1c67916e8622ef137c48398d1efe1177a8e240c8
AdryanAgostinho <50266254+AdryanAgostinho@users.noreply.github.com>
2024-08-13 22:07:18 -0300

    classes de dominio iniciadas

--- src/main/java/com/adryan/helpdesk/domain/enums/Perfil.java
+++ src/main/java/com/adryan/helpdesk/domain/enums/Perfil.java
@@ -1,11 +1,11 @@
 package com.adryan.helpdesk.domain.enums;
 
 public enum Perfil {
-    ADMIN(0,"ROLE_ADMIN"),CLIENTE(1, "ROLE_CLIENTE"),TECNICO(2,"ROLE_TECNICO");
-	
+	ADMIN(0, "ROLE_ADMIN"), CLIENTE(1, "ROLE_CLIENTE"), TECNICO(2, "ROLE_TECNICO");
+
 	private Integer codigo;
 	private String descricao;
-	
+
 	private Perfil(Integer codigo, String descricao) {
 		this.codigo = codigo;
 		this.descricao = descricao;
@@ -18,13 +18,13 @@ public enum Perfil {
 	public String getDescricao() {
 		return descricao;
 	}
-	
+
 	public static Perfil toEnum(Integer cod) {
-		if(cod ==null) {
+		if (cod == null) {
 			return null;
 		}
-		for(Perfil x : Perfil.values()) {
-			if(cod.equals(x.getCodigo())) {
+		for (Perfil x : Perfil.values()) {
+			if (cod.equals(x.getCodigo())) {
 				return x;
 			}
 		}
--- src/main/java/com/adryan/helpdesk/domain/enums/Prioridade.java
+++ src/main/java/com/adryan/helpdesk/domain/enums/Prioridade.java
@@ -1,11 +1,11 @@
 package com.adryan.helpdesk.domain.enums;
 
 public enum Prioridade {
-    BAIXA(0,"BAIXA"), MEDIA(1, "MEDIA"), ALTA(2,"ALTA");
-	
+	BAIXA(0, "BAIXA"), MEDIA(1, "MEDIA"), ALTA(2, "ALTA");
+
 	private Integer codigo;
 	private String descricao;
-	
+
 	private Prioridade(Integer codigo, String descricao) {
 		this.codigo = codigo;
 		this.descricao = descricao;
@@ -18,13 +18,13 @@ public enum Prioridade {
 	public String getDescricao() {
 		return descricao;
 	}
-	
+
 	public static Prioridade toEnum(Integer cod) {
-		if(cod ==null) {
+		if (cod == null) {
 			return null;
 		}
-		for(Prioridade x : Prioridade.values()) {
-			if(cod.equals(x.getCodigo())) {
+		for (Prioridade x : Prioridade.values()) {
+			if (cod.equals(x.getCodigo())) {
 				return x;
 			}
 		}
